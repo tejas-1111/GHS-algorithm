2594325cb64e703816cadde076d2f87f985ce5ae
Tejas Chaudhari (tejas-1111) <1f4e7.tejas.chaudhari@gmail.com>
2022-04-22 23:01:44 +0530

    Started coding

--- main.cpp
+++ main.cpp
@@ -1,12 +1,46 @@
+#include <bits/stdc++.h>
 #include <mpi.h>
 
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-#include <queue>
-#include <vector>
-
 using namespace std;
+
+/*
+1	Blue	9	Light Blue
+2	Green	0	Black
+3	Aqua	A	Light Green
+4	Red	    B	Light Aqua
+5	Purple	C	Light Red
+6	Yellow	D	Light Purple
+7	White	E	Light Yellow
+8	Gray	F	Bright White
+
+Color 0A -> bg Black, fg Light green
+*/
+
+#define INF 100000
+
+/*
+Coding stuff
+
+(INF, INF, INF) being returned as a result of init indicates that no outgoing edge is present
+While merging or absorbing, new fragment root will be the smaller of the two roots
+
+Tag:
+1 -> Init msg -> (root, level)
+2 -> Passing results of probing -> (weight, n1, n2)
+3 -> Test msg -> (root, level)
+4 -> Accept/Reject/Wait -> (0/1/2)
+
+*/
+
+// returns original indexes of elements in a sorted array
+template <typename T>
+vector<size_t> sort_indexes(const vector<T> &v) {
+    vector<size_t> idx(v.size());
+    iota(idx.begin(), idx.end(), 0);
+    stable_sort(idx.begin(), idx.end(), [&v](size_t i1, size_t i2) { return v[i1] < v[i2]; });
+
+    return idx;
+}
 
 int main(int argc, char **argv) {
     int rank, numprocs;
@@ -16,20 +50,102 @@ int main(int argc, char **argv) {
     MPI_Barrier(MPI_COMM_WORLD);               // synchronize all processes
     double start_time = MPI_Wtime();
 
+    //_____________________________________________________________________________________________________________
     // Input and basic constants
     ifstream input(argv[1]);
     int N;
     input >> N;
-    vector<int> nodes_handled(numprocs, N / numprocs);
-    for (int i = 0; i < N % numprocs; ++i) {
-        nodes_handled[i] += 1;
+
+    vector<pair<int, int>> adjlist;  // (weight, endpoint);
+    vector<int> edgetype;            // edgetype = 1 -> branch edge, 0 -> Basic edge, -1 -> Rejected edge
+    int level = 0;
+    int state = 0;  // 0 -> Sleep, 1 -> Find, 2 -> Found
+    int name = rank;
+
+    for (int i = 0; i < rank; ++i) {
+        for (int j = 0; j < N; ++j) {
+            int temp;
+            input >> temp;
+        }
     }
-    for (int i = 1; i < numprocs; ++i) {
-        nodes_handled[i] += nodes_handled[i - 1];
+
+    for (int i = 0; i < N; ++i) {
+        int temp;
+        input >> temp;
+        if (temp != INF) {
+            adjlist.push_back({temp, i});
+            edgetype.push_back(0);
+        }
     }
-    for (int i = 0; i < numprocs; ++i) {
-        cout << nodes_handled[i] << endl;
+
+    for (int i = rank + 1; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            int temp;
+            input >> temp;
+        }
     }
-    
+    //_____________________________________________________________________________________________________________
+    while (true) {
+        // Initiate stage
+        if (rank == name) {
+            // Node is a root
+
+            // Send initiate along branch edges
+            for (int i = 0; i < edgetype.size(); ++i) {
+                if (edgetype[i] == 1) {
+                    system("Color 01");
+                    cout << rank << " sending initiate message to " << adjlist[i].second << endl;
+                    string msg = "Init";
+                    MPI_Send(&msg, msg.length(), MPI_CHAR, adjlist[i].second, 1, MPI_COMM_WORLD);
+                }
+            }
+
+            // Probe along basic edges to find minimum outgoing edge
+            vector<pair<int, pair<int, int>>> results;
+            for (auto i : sort_indexes(adjlist)) {
+                if (edgetype[i] == 0) {
+                    string msg = "Test";
+                    MPI_Request request;
+                    system("Color 02");
+                    cout << rank << " sending test to " << adjlist[i].second << endl;
+                    MPI_Isend(&msg, msg.length(), MPI_CHAR, adjlist[i].second, 3, MPI_COMM_WORLD, &request);
+                    MPI_Wait(&request, MPI_STATUS_IGNORE);
+                    MPI_Status status;
+                    MPI_Recv(&msg, msg.length(), MPI_CHAR, MPI_ANY_SOURCE, 3, MPI_COMM_WORLD, &status);
+                    if ()
+                    MPI_Irecv(&msg, msg.length(), MPI_CHAR, adjlist[i].second, 4, MPI_COMM_WORLD, &request);
+                    MPI_Wait()
+                }
+            }
+
+            // Receive results
+
+            // Decide the minimum outgoing edge
+
+        } else {
+            // Not a root
+            // Wait for initiate
+            string msg = "    ";
+            MPI_Status status;
+            MPI_Recv(&msg, msg.length(), MPI_CHAR, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
+            system("Color 01");
+            cout << rank << " received initiate message from " << status.MPI_SOURCE << endl;
+
+            // Forward init along branch edges
+            for (int i = 0; i < edgetype.size(); ++i) {
+                if (edgetype[i] == 1 && adjlist[i].second != status.MPI_SOURCE) {
+                    system("Color 01");
+                    cout << rank << " forwarding initiate message to " << adjlist[i].second << endl;
+                    string msg = "Init";
+                    MPI_Send(&msg, msg.length(), MPI_CHAR, adjlist[i].second, 1, MPI_COMM_WORLD);
+                }
+            }
+
+            // Probe along basic edges to determine minimum outgoing edge
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Finalize();
     return 0;
 }
